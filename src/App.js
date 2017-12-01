99560d5ed64713a14822e36141a6203a13779bfd
Irene Mizus <sirius1986@bk.ru>
2017-12-01 14:00:37 +0300

    Last issues performed. Last update - 01.12.2017

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import { Switch, Route, withRouter } from 'react-router-dom';
 
 import './App.css';
