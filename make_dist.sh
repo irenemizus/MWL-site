86d0422783a5b399843e51fa8e41ebe19be9ee89
Irene Mizus <irenemizus@gmail.com>
2019-03-14 14:21:47 +0300

    make_dist.sh changed correspondingly.

--- make_dist.sh
+++ make_dist.sh
@@ -6,7 +6,7 @@ echo Copying public...
 cp public/*.json dist/
 cp -r public/Papers_pdf dist/
 cp -r public/img dist/
-cp -r public/mht dist/
+cp -r public/water_dimer_press_pdf dist/
 
 echo Building react part...
 ./node_modules/.bin/webpack --optimize-minimize
