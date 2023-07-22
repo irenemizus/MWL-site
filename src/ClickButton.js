321cf1c7947b61ae293b98f77624101253b825c4
Irene Mizus <irene.mizus@gmail.com>
2023-07-22 16:12:49 +0300

    The updated version of the MWL-site as for July, 2023. TODO: to add style to switching part of the Staff ptoto for the "people" head page.

---
+++ src/ClickButton.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import renderHTML from 'react-render-html';
+
+import './ArticlePagesList.css';
+
+export default class ClickButton extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			title: props.title,
+			handler: props.handler
+		}
+	}
+	
+	render() {
+		var buttonStyle;
+	
+		if (this.props.active || this.props.linkfrom) {
+			buttonStyle = "page_button_active";
+		} else {
+			if (this.props.colors === "dark") buttonStyle = "page_button_dark";
+			else buttonStyle = "page_button_light";
+		}
+
+		var title = renderHTML(this.state.title)
+		var boxStyle = { 
+			"width": "100%"
+		}
+		if (title.length <= 4) {
+			boxStyle.maxWidth = "35pt";
+		} else if (title.length === 9 || title.length === 10) {
+			boxStyle.maxWidth = "74pt";
+		} else {
+			boxStyle.maxWidth = "150pt";
+		}		
+		
+		var linkStyle = {"display": "block", "textAlign": "center"}
+		
+		if (this.props.active) {
+			return (
+				<div style={boxStyle} className={buttonStyle}>
+					<div style={linkStyle}>{title}</div>
+				</div>
+			)
+		} else {
+			return (
+				<div style={boxStyle} className={buttonStyle}>
+					<div onClick={this.state.handler} style={linkStyle}>{title}</div>
+				</div>
+			)
+		}
+
+	}
+}
--- src/StaffPage.js
+++ src/StaffPage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import StaffCatButtons from './StaffCatButtons';
 
 import './Page.css';
-import PageButton from "./PageButton";
+import ClickButton from "./ClickButton";
 
 export default class StaffPage extends Component {
 	constructor(props) {
@@ -18,35 +18,49 @@ export default class StaffPage extends Component {
 	}
 
 	render() {
-//  		window.scrollTo(0, 0);
+		// window.scrollTo(0, 0);
 		let photoButtons = [];
 		let image;
 		if (this.props.photos) {
 			let lastImg = this.props.photos[this.state.selectedKey];
 			let lastUrl = lastImg.url;
-			image = <img className="foto_team" alt="" src={'/img/foto/' + lastUrl}></img>;
 			for (let i in this.state.photoKeys) {
 				let curImgKey = this.state.photoKeys[i]
+				console.log(curImgKey);
 				let buttonView;
 				let prefix = '/people/';
 
-				if (this.props.location.pathname === prefix + curImgKey) {
-					buttonView = <PageButton active={true} key={i} index={curImgKey} title={curImgKey}
-											 prefix={prefix}/>;
-					image = <img className="foto_team" alt="" src={'/img/foto/' + this.props.photos[i].url}></img>
-				} else {
-					buttonView = <PageButton colors={this.props.colors} key={i} index={curImgKey}
-											 title={curImgKey} prefix={prefix}/>;
+				//let myTabClick = tabClick.bind(this, curImgKey);
+				//if (this.props.location.pathname === prefix + curImgKey) {
+				let tabClick = () => {
+					console.log(curImgKey)
+					this.setState(
+						{
+							photoKeys: this.state.photoKeys,
+							selectedKey: curImgKey
+						}
+					);
 				}
+
+				buttonView = <ClickButton title={curImgKey} active={this.state.selectedKey === curImgKey} handler={tabClick}/>;
+
+					//image = <div><img className="foto_team" alt="" src={'/img/foto/' + this.props.photos[i].url}></img></div>;
+				//}
+				// else {
+				// 	buttonView = <PageButton colors={this.props.colors} key={i} index={curImgKey}
+				// 							 title={curImgKey} prefix={prefix}/>;
+				// }
 
 				photoButtons.push(buttonView);
 			}
+			image = <div><img className="foto_team" alt="" src={'/img/foto/' + lastUrl}></img></div>;
 		}
 
 		return (
 				<div className="foto_frame">
-					 <div>{image}</div>
-					 <StaffCatButtons colors="light" staffCatTitles={this.props.staffCatTitles} location={this.props.location} />
+					<div>{photoButtons}</div>
+					<div>{image}</div>
+					<StaffCatButtons colors="light" staffCatTitles={this.props.staffCatTitles} location={this.props.location} />
 				</div>
 		);
   	}
